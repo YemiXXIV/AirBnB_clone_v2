73a32d9d4be2e3178891493a5d6ab7fad03a7308
Mustapha <mustapha.yinusa@yahoo.com>
2024-05-16 16:15:13 +0100

    testing something

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -16,9 +16,8 @@ def do_pack():
     """
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
         file_name = "versions/web_static_{}.tgz".format(date)
+        local("mkdir -p versions")
         local("tar -cvzf {} web_static".format(file_name))
         return file_name
 
