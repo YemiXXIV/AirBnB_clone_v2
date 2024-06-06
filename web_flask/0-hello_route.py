3a0dafaed19449780631bb1181b3036d110e81b5
Mustapha <mustapha.yinusa@yahoo.com>
2024-06-06 10:41:25 +0100

    Task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """
-Routes for / 
+Routes for /
 """
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
@@ -12,6 +13,7 @@ def hello_hbnb():
     Route to /
     """
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
